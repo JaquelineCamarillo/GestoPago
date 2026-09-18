7ab0fb60a89adb8d2a74bdc4c6f447565d35c3ea
Jaqueline Camarillo <juanaolaez95@gmail.com>
2026-09-17 23:00:23 -0600

    feat(gestopago): tabla, entidad y repositorio para el catalogo local de productos

---
+++ src/main/java/com/proyecto/servicios/repositorys/gestopago/GestoPagoProductoRepository.java
@@ -0,0 +1,16 @@
+package com.proyecto.servicios.repositorys.gestopago;
+
+import com.proyecto.servicios.entity.gestopago.GestoPagoProducto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface GestoPagoProductoRepository extends JpaRepository<GestoPagoProducto, Integer> {
+
+    Optional<GestoPagoProducto> findByIdProductoAndIdServicio(Integer idProducto, Integer idServicio);
+
+    List<GestoPagoProducto> findByActivoTrue();
+}
